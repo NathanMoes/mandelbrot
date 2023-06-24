bf601b23dd1a5fe1dd028a34ebc34ef195fdfd20
Nathan Moes <nathanmoes7@gmail.com>
2023-06-23 18:44:56 -0700

    added render function for greyscale mandelbrot

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,57 @@ fn parse_complex(s: &str) -> Option<Complex<f64>>{
     }
 }
 
+/// given row, column of pixel in output image return the corespoing point in the complex plane
+/// 'bounds' is a pair giving the width and hiehgt of the image in pixels
+/// 'pixel' is a (column, row) pair indicating a particular pixel in taht image.
+/// the 'upper_left' and 'lower_right' parameters are points on the complex plan designating the area our image covers/
+fn pixel_to_point(bounds: (usize, usize),
+    pixel: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>)-> Complex<f64>{
+        let (width, height) = (lower_right.re - upper_left.re,
+            upper_left.im - lower_right.im);
+        Complex{
+            re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+            im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+        }
+    }
+
+
+/// Render a rectangle of the mandelbrot set into a buffer of pixels.
+/// 
+/// The 'bounds' argument gives the width and height of the buffer 'pixels'
+/// which holds one grayscale pixel per byte. The 'upper_left' and 'lower_right'
+/// arguments specify points on the complex plane correspoding to the upper-left 
+/// and lower right corners of the pixel buffer.
+fn render(pixels: &mut[u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>){
+        assert!(pixels.len() == bounds.0 * bounds.1);
+
+        for row in 0..bounds.1{
+            for column in 0..bounds.0{
+                let point = pixel_to_point(bounds, (column, row),
+                    upper_left, lower_right);
+                pixels[row * bounds.0 + column] =
+                    match escape_time(point, 255){
+                        None => 0,
+                        Some(count) => 255 - count as u8
+                    };
+            }
+        }
+    }
+
+
+#[test]
+fn test_pixel_to_point(){
+    assert_eq!(pixel_to_point((100, 100), (25, 75),
+        Complex {re: -1.0, im: 1.0},
+        Complex {re: 1.0, im: -1.0}),
+        Complex {re: -0.5, im: -0.5});
+}
+
 #[test]
 fn test_parse_complex(){
     assert_eq!(parse_complex("1.25,-0.0625"),
